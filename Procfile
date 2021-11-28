50c03a2d241373ffc1625e08fd07f738c16af2e6
ZansLord <shikaizaidan@gmail.com>
2021-11-29 01:47:44 +0700

    kont

---
+++ Procfile
@@ -0,0 +1,4 @@
+worker: node index.js
+worker1: npm install && node index.is
+worker2: npm start
+worker3: npm install && npm start
---
+++ database/menu.js
@@ -0,0 +1,253 @@
+const menunya = (prefix) => {
+	return`┌━━֍  〔 🚀*${NamaBot}* 🚀〕
+│
+│▀█ ▄▀█ █▄ █ █▀▀ █   █▀█ █▀█ █▀▄
+│█▄ █▀█ █ ▀█ ▄▄█ █▄▄ █▄█ █▀▄ █▄▀
+│
+├━━֍  〔 🚀 *OWNER* 🚀〕
+├ᐷ 丂千 .addrespon
+├ᐷ 丂千 .autorespon 1/0
+├ᐷ 丂千 .bc
+├ᐷ 丂千 .bcaudio
+├ᐷ 丂千 .clearall
+├ᐷ 丂千 .creategrup
+├ᐷ 丂千 .culik
+├ᐷ 丂千 .delcmd
+├ᐷ 丂千 .exif
+├ᐷ 丂千 .getscmd
+├ᐷ 丂千 .jadibot
+├ᐷ 丂千 .listcmd
+├ᐷ 丂千 .mute
+├ᐷ 丂千 .nsfw 1/0
+├ᐷ 丂千 .offline
+├ᐷ 丂千 .online
+├ᐷ 丂千 .public
+├ᐷ 丂千 .restart
+├ᐷ 丂千 .tobc
+├ᐷ 丂千 .self
+├ᐷ 丂千 .setbio
+├ᐷ 丂千 .setcmd
+├ᐷ 丂千 .setprefix
+├ᐷ 丂千 .setprofile
+├ᐷ 丂千 .shutdown
+├ᐷ 丂千 .stopjadibot
+└━━֍  
+┌━━֍  〔 🚀 *UPLOAD SW* 🚀〕
+├ᐷ 丂千 .spamsw
+├ᐷ 丂千 .upswteks
+├ᐷ 丂千 .upswlokasi
+├ᐷ 丂千 .upswsticker
+├ᐷ 丂千 .upswaudio
+├ᐷ 丂千 .upswvoice
+├ᐷ 丂千 .upswvideo
+├ᐷ 丂千 .upswgif
+├ᐷ 丂千 .upswimage
+└━━֍  
+┌━━֍  〔 🚀 *GRUP* 🚀〕
+├ᐷ 丂千 .ad ( reply pesan )
+├ᐷ 丂千 .add 628..dst
+├ᐷ 丂千 .antibug 1/0
+├ᐷ 丂千 .antidelete 1/0
+├ᐷ 丂千 .antilink 1/0
+├ᐷ 丂千 .caripesan
+├ᐷ 丂千 .fitnah
+├ᐷ 丂千 .getdeskgc
+├ᐷ 丂千 .grup
+├ᐷ 丂千 .getpp @tagtarget
+├ᐷ 丂千 .hacked
+├ᐷ 丂千 .hidetag
+├ᐷ 丂千 .infouser (reply pesan)
+├ᐷ 丂千 .kontak
+├ᐷ 丂千 .kick @tagmember
+├ᐷ 丂千 .kik  ( reply pesan )
+├ᐷ 丂千 .linkgrup
+├ᐷ 丂千 .listadmin
+├ᐷ 丂千 .listonline
+├ᐷ 丂千 .revoke
+├ᐷ 丂千 .setdeskgc
+├ᐷ 丂千 .setnamegc
+├ᐷ 丂千 .sider
+├ᐷ 丂千 .sticktag
+├ᐷ 丂千 .spam
+├ᐷ 丂千 .tag
+├ᐷ 丂千 .tagall
+├ᐷ 丂千 .tagme
+├ᐷ 丂千 .tospam
+├ᐷ 丂千 .totag
+└━━֍  
+┌━━֍  〔 🚀 *MAKER* 🚀〕
+├ᐷ 丂千 .attp
+├ᐷ 丂千 .battlefield
+├ᐷ 丂千 .blackbird
+├ᐷ 丂千 .codetxt
+├ᐷ 丂千 .coffeecup
+├ᐷ 丂千 .coffeecup2
+├ᐷ 丂千 .dance
+├ᐷ 丂千 .express
+├ᐷ 丂千 .foliokanan
+├ᐷ 丂千 .foliokiri
+├ᐷ 丂千 .glitch
+├ᐷ 丂千 .glow
+├ᐷ 丂千 .goldbutton
+├ᐷ 丂千 .golden
+├ᐷ 丂千 .googletxt
+├ᐷ 丂千 .hallowen
+├ᐷ 丂千 .hengker (reply Gambarnya)
+├ᐷ 丂千 .hengker2 (reply Gambarnya)
+├ᐷ 丂千 .maker2d2
+├ᐷ 丂千 .maker2d3
+├ᐷ 丂千 .maker2d4
+├ᐷ 丂千 .maker3d
+├ᐷ 丂千 .maker3d2
+├ᐷ 丂千 .maker3d3
+├ᐷ 丂千 .maker3d4
+├ᐷ 丂千 .matrix
+├ᐷ 丂千 .neon
+├ᐷ 丂千 .nulis
+├ᐷ 丂千 .nuliskiri
+├ᐷ 丂千 .nuliskanan
+├ᐷ 丂千 .nostonk (reply Gambarnya)
+├ᐷ 丂千 .patrick 
+├ᐷ 丂千 .pornhub (teks|teks)
+├ᐷ 丂千 .qrcode
+├ᐷ 丂千 .readmore
+├ᐷ 丂千 .rip (reply Gambarnya)
+├ᐷ 丂千 .semoji
+├ᐷ 丂千 .shareloc
+├ᐷ 丂千 .silverbutton
+├ᐷ 丂千 .stonk (reply Gambarnya)
+├ᐷ 丂千 .spiderman
+├ᐷ 丂千 .spongebob (reply Gambarnya)
+├ᐷ 丂千 .summer
+├ᐷ 丂千 .tahta
+├ᐷ 丂千 .text3d
+├ᐷ 丂千 .tinyurl
+├ᐷ 丂千 .toimg
+├ᐷ 丂千 .tomp3
+├ᐷ 丂千 .totext
+├ᐷ 丂千 .tourl
+├ᐷ 丂千 .transformer
+├ᐷ 丂千 .ttp
+├ᐷ 丂千 .tts
+├ᐷ 丂千 .vampire
+├ᐷ 丂千 .viewonce
+├ᐷ 丂千 .warrior
+├ᐷ 丂千 .wooden
+┌━━֍   〔 🚀 *DOWNLOADER* 🚀〕
+├ᐷ 丂千 .dafontdl (link Dafont)
+├ᐷ 丂千 .igstory
+├ᐷ 丂千 .igdl
+├ᐷ 丂千 .mediafire
+├ᐷ 丂千 .play
+├ᐷ 丂千 .tiktok
+├ᐷ 丂千 .ytmp3
+├ᐷ 丂千 .ytmp4 
+├ᐷ 丂千 .ytdownload 
+└━━֍  
+┌━━֍  〔 🚀 *FUN* 🚀〕
+├ᐷ 丂千 .artimimpi
+├ᐷ 丂千 .bilangangka
+├ᐷ 丂千 .bisakah
+├ᐷ 丂千 .cekpintar
+├ᐷ 丂千 .cekganteng
+├ᐷ 丂千 .cekcantik
+├ᐷ 丂千 .cekmati
+├ᐷ 丂千 .dadu
+├ᐷ 丂千 .dare
+├ᐷ 丂千 .delttc
+├ᐷ 丂千 .fdeface (error)
+├ᐷ 丂千 .fitnahpc
+├ᐷ 丂千 .hbd
+├ᐷ 丂千 .herolist
+├ᐷ 丂千 .herodetail
+├ᐷ 丂千 .jadian
+├ᐷ 丂千 .kapankah
+├ᐷ 丂千 .slot
+├ᐷ 丂千 .suit
+├ᐷ 丂千 .tictactoe
+├ᐷ 丂千 .truth
+└━━֍  
+┌━━֍  〔 🚀 *STICKER* 🚀〕
+├ᐷ 丂千 .sticker
+├ᐷ 丂千 .swm
+├ᐷ 丂千 .takestick
+├ᐷ 丂千 .colong
+└━━֍  
+┌━━֍  〔 🚀 *SEARCH* 🚀〕
+├ᐷ 丂千 .brainly
+├ᐷ 丂千 .dafontsearch (nama Font)
+├ᐷ 丂千 .githubstalk
+├ᐷ 丂千 .gimage
+├ᐷ 丂千 .google
+├ᐷ 丂千 .infogempa
+├ᐷ 丂千 .lirik
+├ᐷ 丂千 .pinterest
+├ᐷ 丂千 .playstore
+├ᐷ 丂千 .resepmasakan
+├ᐷ 丂千 .wiki
+├ᐷ 丂千 .ytsearch
+└━━֍  
+┌━━֍  〔 🚀 *WAR* 🚀〕
+├ᐷ 丂千 .xbug
+├ᐷ 丂千 .bugbutton
+├ᐷ 丂千 .bugrow
+├ᐷ 丂千 .bugcombine
+├ᐷ 丂千 .tesbug
+├ᐷ 丂千 .bugpc
+├ᐷ 丂千 .bugtroli2
+├ᐷ 丂千 .bugtroli3
+├ᐷ 丂千 .virtex
+├ᐷ 丂千 .tovirvid
+├ᐷ 丂千 .tovirgam
+├ᐷ 丂千 .bugkatalog (perbaikan)
+├ᐷ 丂千 .bugloc
+├ᐷ 丂千 .buglokasi
+├ᐷ 丂千 .x
+└━━֍  
+┌━━֍  〔 🚀 *STORAGE* 🚀〕
+├ᐷ 丂千 .addimage
+├ᐷ 丂千 .addsticker
+├ᐷ 丂千 .addvn
+├ᐷ 丂千 .delimage
+├ᐷ 丂千 .delsticker
+├ᐷ 丂千 .delvn
+├ᐷ 丂千 .liststicker
+├ᐷ 丂千 .listvn
+├ᐷ 丂千 .listimage
+└━━֍  
+┌━━֍  〔 🚀 *ASUPAN* 🚀〕
+├ᐷ 丂千 .asupan
+└━━֍  
+┌━━֍  〔 🚀 *TOLLS* 🚀〕
+├ᐷ 丂千 .get
+├ᐷ 丂千 .kalkulator
+└━━֍  
+┌━━֍  〔 🚀 *TENTANG BOT* 🚀〕
+⏐
+├ᐷ 丂千 .listblock
+├ᐷ 丂千 .baileys
+├ᐷ 丂千 .delete
+├ᐷ 丂千 .getcaption
+├ᐷ 丂千 .listgrup
+├ᐷ 丂千 .owner
+├ᐷ 丂千 .q
+├ᐷ 丂千 .report (Pesan Errornya)
+├ᐷ 丂千 .rules
+├ᐷ 丂千 .runtime
+├ᐷ 丂千 .sewa
+├ᐷ 丂千 .speed
+├ᐷ 丂千 .status
+├ᐷ 丂千 .tes
+└━━֍  
+┌━━֍  〔 🚀 *CONVERT* 🚀〕
+⏐
+├ᐷ 丂千 .balik (reply audio)
+├ᐷ 丂千 .bass (reply audio)
+├ᐷ 丂千 .detikvn (reply audio)
+├ᐷ 丂千 .detikvideo (reply audio)
+├ᐷ 丂千 .gemuk (reply audio)
+├ᐷ 丂千 .robot (reply audio)
+⏐
+└━━֍  〔 🚀 *${NamaBot}* 🚀〕`
+}
+exports.menunya = menunya
